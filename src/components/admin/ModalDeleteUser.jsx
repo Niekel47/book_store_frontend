de3cece417a4d961b06a3637d6923c6a12c52039
Niekel47 <kien742002@gmail.com>
2024-05-08 07:39:08 +0700

    Update

--- src/components/admin/ModalDeleteUser.jsx
+++ src/components/admin/ModalDeleteUser.jsx
@@ -32,7 +32,8 @@ const ModalDeleteUser = (props) => {
             className="px-4 py-2 text-white bg-gray-500 rounded hover:bg-gray-400"
             onClick={handleClose}
           >
-            Hủyyyy
+            Hủyyyyy
+            
           </Button>
           <Button
             className="px-4 py-2 text-white bg-red-500 rounded hover:bg-red-400 ml-2"
